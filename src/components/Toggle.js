c78d010ebadfadc02e0b1d27b37c06d7da36b007
John J Setlock II <johnsetlock@gmail.com>
2022-08-12 08:04:51 -0500

    Completed Assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,27 @@
-import React from "react";
+
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  const [isOn,setIsOn]=useState(false);  //set initial state
+
+  function handleClick(){
+
+    setIsOn(isOn=> !isOn)
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+  return <button onClick={handleClick}>{isOn?"ON":"OFF"}</button>;
 }
 
 export default Toggle;
